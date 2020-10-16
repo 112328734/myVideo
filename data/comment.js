49a37660335e54f5f316ff4823e869c057e2c108
wanggongle <wanggongle@juntu.com>
2020-10-16 10:51:41 +0800

    init

---
+++ data/comment.js
@@ -0,0 +1,15 @@
+var commentData = {
+	"count": 1,
+	"page": 1,
+	"data": [{
+			"name": "AAA",
+			"img": "./images/shuijiao.jpg",
+			"comment": "哈哈，这是下拉列表"
+		},
+		{
+			"name": "BBB",
+			"img": "./images/shuijiao.jpg",
+			"comment": "哈哈，这是下拉列表BBB"
+		}
+	]
+}
---
+++ data/list.js
@@ -0,0 +1,95 @@
+var videolistsData = {
+	"count": 1,
+	"page": 1,
+	"data": [{
+			"video_url": "http://video.izuiyou.com/zyvd/94/56/378b-5962-11ea-abee-00163e143aa6",
+			"comment_num": 312,
+			"is_like": 1,
+			"like_num": 111,
+			"goods_img":"./images/yuantiao.jpg",
+			"goods_type":1,
+			"goods_title":"测试商品1",
+			"goods_sub_title":"测试商品1",
+			"goods_mark_price":"200",
+			"goods_sale_price":"480",
+			"goods_link":'',
+			"comment_list":{
+					"count": 1,
+					"page": 1,
+					"data": [{
+							"name": "AAA",
+							"img": "./images/shuijiao.jpg",
+							"comment": "哈哈，这是下拉列表1",
+							'publish_time':'刚刚'
+						},
+						{
+							"name": "BBB",
+							"img": "./images/shuijiao.jpg",
+							"comment": "哈哈，这是下拉列表BBB1",
+							'publish_time':'刚刚'
+						}
+					]
+				}
+			},
+		{
+			"video_url": "http://res.imtt.qq.com/qqbrowser/js/advideo.mp4",
+			"comment_num": 222,
+			"is_like": 2,
+			"like_num": 222,
+			"goods_img":"./images/yuantiao.jpg",
+			"goods_type":1,
+			"goods_title":"测试商品2",
+			"goods_sub_title":"",
+			"goods_mark_price":"100",
+			"goods_sale_price":"50",
+			"goods_link":'',
+			"comment_list":{
+					"count": 1,
+					"page": 1,
+					"data": [{
+							"name": "AAA",
+							"img": "./images/shuijiao.jpg",
+							"comment": "哈哈，这是下拉列表2",
+							'publish_time':'刚刚'
+						},
+						{
+							"name": "BBB",
+							"img": "./images/shuijiao.jpg",
+							"comment": "哈哈，这是下拉列表BBB2",
+							'publish_time':'刚刚'
+						}
+					]
+				}
+		},
+		{
+			"video_url": "http://res.imtt.qq.com/qqbrowser/js/advideo.mp4",
+			"comment_num": 112,
+			"is_like": 2,
+			"like_num": 333,
+			"goods_img":"./images/yuantiao.jpg",
+			"goods_type":2,
+			"goods_title":"测试标题",
+			"goods_sub_title":"测试副标题",
+			"goods_mark_price":"",
+			"goods_sale_price":"",
+			"goods_link":'',
+			"comment_list":{
+					"count": 1,
+					"page": 1,
+					"data": [{
+							"name": "AAA",
+							"img": "./images/shuijiao.jpg",
+							"comment": "哈哈，这是下拉列表1",
+							'publish_time':'刚刚'
+						},
+						{
+							"name": "BBB",
+							"img": "./images/shuijiao.jpg",
+							"comment": "哈哈，这是下拉列表BBB2",
+							'publish_time':'刚刚'
+						}
+					]
+			}
+		}
+	]
+}
